5245b5bfdd67eec0683ccbc40d051f4cd1242726
luisKisters <luis.w.kisters@gmail.com>
2023-04-12 22:18:46 +0200

    Changed model

--- chat/models.py
+++ chat/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 
 # Create your models here.
-class Chat(models.Model):
-    user = models.CharField(max_length=30)
-    message = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class User(models.Model):
+    username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(max_length=254, unique=True)
+    password = models.CharField(max_length=128)
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    chat_room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
