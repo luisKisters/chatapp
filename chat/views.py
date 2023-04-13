b9657e93357f1f015fa99c6cde12bd517e7af30c
luisKisters <luis.w.kisters@gmail.com>
2023-04-13 02:13:42 +0200

    Added React build to view

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import ChatRoom, User, Message
 
 # Create your views here.
+
+def index_view(request, *args, **kwargs):
+    return render(request, "index.html", context)
